7201a9aa8f2cfea56156604c3a1b96deb7e9b5e4
Steve Eckhardt <steve@eckop.com>
2025-01-09 18:46:19 -0600

    still trying to figure out exception handling

--- TestMKL/MainWindow.xaml.cpp
+++ TestMKL/MainWindow.xaml.cpp
@@ -4,6 +4,9 @@
 #include "MainWindow.g.cpp"
 #endif
 #include "mkl.h"
+#include <exception>
+// Standard SYCL header
+//#include "compiler\2025.0\include\sycl\CL\sycl.hpp"
 
 using namespace winrt;
 using namespace Microsoft::UI::Xaml;
@@ -21,7 +24,13 @@ namespace winrt::TestMKL::implementation
         // Example: Compute the dot product of two vectors
         double x[3] = { 1.0, 2.0, 3.0 };
         double y[3] = { 4.0, 5.0, 6.0 };
-        double rv = cblas_ddot(3, x, 1, y, 1);
+        double rv;
+        try {
+            rv = cblas_ddot(3, x, 1, y, 1);
+		}
+		catch (const std::exception& e) {
+			OutputDebugString((L"MKL exception: " + to_hstring(e.what()) + L"\n").c_str());
+		}
         OutputDebugString((L"Dot product: " + std::to_wstring(rv) + L"\n").c_str());
         result().Text(winrt::hstring{ std::to_wstring(rv) });
     }
